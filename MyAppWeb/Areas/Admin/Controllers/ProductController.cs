ed0e07d1871ad47aa56d83e834c04cb104d32c07
Fetter Dávid <fetter.david00@gmail.com>
2022-03-25 00:02:22 +0100

    Add Product View-Model

--- MyAppWeb/Areas/Admin/Controllers/ProductController.cs
+++ MyAppWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using MyApp.DataAccess.Repository.IRepository;
+using MyApp.Models;
+using MyApp.Models.ViewModels;
 
 namespace MyAppWeb.Areas.Admin.Controllers
 {
@@ -15,6 +18,53 @@ namespace MyAppWeb.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            ProductVM productVM = new()
+            {
+                product = new(),
+                categoryList = _unitOfWork.Category.GetAll().Select(
+                u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                coverTypeList = _unitOfWork.CoverType.GetAll().Select(
+                u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                })
+            };
+
+            if (id == null || id == 0)
+            {
+                //Create
+                return View(productVM);
+            }
+            else
+            {
+                //Update
+
+            }
+
+            return View(productVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(ProductVM obj, IFormFile file)
+        {
+           if(ModelState.IsValid)
+           {
+                //_unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                return Redirect("Index");
+           }
+
+            return View(obj);
         }
     }
 }
